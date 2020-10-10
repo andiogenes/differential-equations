cea629e7a4f8b66bfad27f98bb7e9f82ba9ac67c
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-10-10 22:00:15 +0700

    create programm entry

--- shooting-method/app/Main.hs
+++ shooting-method/app/Main.hs
@@ -1,10 +1,28 @@
 module Main where
 
 import Pair (Pair (..))
-import ShootingMethod (solve, x0, xn)
+import ShootingMethod (solve, phi)
 
-solution from to h0 eps = 
-    solve from to h0 eps (Pair 10 10) 
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Diagrams
+
+solution = solve from to step eps (Pair 10 10)
+    where
+        from    = -3
+        to      = -0.5
+        step    = 0.001
+        eps     = 0.0000001 
+
+table = reverse $ phi s step
+    where
+        (s, _)  = solution
+        step    = 0.001
 
 main :: IO ()
-main = putStr $ show $ solution (-3) (-0.5) 0.001 0.0000001
+main = toFile def "solution.svg" $ do
+    layout_title .= "Solution"
+    setColors [opaque blue, opaque green]
+    plot (line "u" [u])
+    plot (line "u'" [u'])
+    where
+        (u, u') = unzip $ map (\(x, Pair u u') -> ((x, u),(x, u'))) table
